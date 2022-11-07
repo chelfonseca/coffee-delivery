a406d7626717ffadeddd900c3d9edd34c40333cc
Michel Fonseca <michelbeserradafonseca@gmail.com>
2022-11-07 16:40:50 -0300

    Global style setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
 
 function App() {
 
 
   return (
-    <h1>Coffe Delivery</h1>
+    <ThemeProvider theme={defaultTheme}>
+        <h1>Coffe Delivery</h1>
+        <GlobalStyle/>
+    </ThemeProvider>
+    
   )
 }
 
