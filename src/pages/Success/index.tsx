32a490b8292bf1917cf5c5dc95414667c9d30303
Michel Fonseca <michelbeserradafonseca@gmail.com>
2022-11-28 17:57:09 -0300

    Success component

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,3 +1,67 @@
+import {
+  IllustrationContainer,
+  ItemIconLocal,
+  ItemIconPayment,
+  ItemIconTime,
+  ItemText,
+  OrderInfoContainer,
+  OrderInfoContent,
+  OrderInfoItem,
+  SuccessBody,
+  SuccessContainer,
+  SuccessHeader,
+} from './styles'
+
+import { MapPin, Timer, CurrencyDollar } from 'phosphor-react'
+
+import SuccessIllustration from '../../assets/SuccessIllustration.svg'
 export function Success() {
-  return <h1>Success</h1>
+  return (
+    <SuccessContainer>
+      <SuccessHeader>
+        <h2>uhu! Pedido confirmado</h2>
+        <p>Agora é só aguardar que logo o café chegará até você</p>
+      </SuccessHeader>
+      <SuccessBody>
+        <OrderInfoContainer>
+          <OrderInfoContent>
+            <OrderInfoItem>
+              <ItemIconLocal>
+                <MapPin size={16} weight="fill" />
+              </ItemIconLocal>
+              <ItemText>
+                <p>
+                  Entrega em <b>Rua João Daniel Martinelli, 102</b>
+                </p>
+                <p>Farrapos - Porto Alegre, RS</p>
+              </ItemText>
+            </OrderInfoItem>
+            <OrderInfoItem>
+              <ItemIconTime>
+                <Timer size={16} weight="fill" />
+              </ItemIconTime>
+              <ItemText>
+                <p>Previsão de entrega</p>
+                <p>
+                  <b>20 min - 30 min</b>
+                </p>
+              </ItemText>
+            </OrderInfoItem>
+            <OrderInfoItem>
+              <ItemIconPayment>
+                <CurrencyDollar size={16} weight="fill" />
+              </ItemIconPayment>
+              <ItemText>
+                <p>Pagamento na entrega</p>
+                <p>
+                  <b>Cartão de crédito</b>
+                </p>
+              </ItemText>
+            </OrderInfoItem>
+          </OrderInfoContent>
+        </OrderInfoContainer>
+        <IllustrationContainer src={SuccessIllustration} />
+      </SuccessBody>
+    </SuccessContainer>
+  )
 }
---
+++ src/pages/Success/styles.ts
@@ -0,0 +1,74 @@
+import styled from 'styled-components'
+
+export const SuccessContainer = styled.main`
+  width: 100%;
+  height: 100vh;
+  background-color: ${(props) => props.theme.background};
+`
+export const SuccessHeader = styled.header`
+  width: auto;
+  margin: 3rem 0;
+  h2 {
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 2rem;
+    color: ${(props) => props.theme['yellow-dark']};
+  }
+
+  p {
+    color: ${(props) => props.theme['base-subtitle']};
+  }
+`
+export const SuccessBody = styled.body`
+  width: auto;
+  display: flex;
+  justify-content: space-between;
+`
+export const OrderInfoContainer = styled.div`
+  display: flex;
+  width: 33rem;
+  height: 17rem;
+  border-radius: 0.5rem;
+  border-top-right-radius: 3rem;
+  border-bottom-left-radius: 3rem;
+  padding: 1px;
+  background: linear-gradient(
+    135deg,
+    ${(props) => props.theme['yellow-dark']},
+    ${(props) => props.theme['purple-dark']}
+  );
+`
+export const OrderInfoContent = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: ${(props) => props.theme.background};
+  border-radius: inherit;
+  padding: 3rem;
+`
+export const OrderInfoItem = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 1rem;
+`
+export const ItemIcon = styled.div`
+  width: 2rem;
+  height: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${(props) => props.theme.white};
+  border-radius: 100%;
+`
+export const ItemIconLocal = styled(ItemIcon)`
+  background-color: ${(props) => props.theme.purple};
+`
+export const ItemIconTime = styled(ItemIcon)`
+  background-color: ${(props) => props.theme.yellow};
+`
+export const ItemIconPayment = styled(ItemIcon)`
+  background-color: ${(props) => props.theme['yellow-dark']};
+`
+
+export const ItemText = styled.div``
+export const IllustrationContainer = styled.img``
